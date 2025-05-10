ef7b8e529c045ed1c92b5db9e7a7b1bce1ff5632
Gehad Shalaby <119525675+Gehad555@users.noreply.github.com>
2025-05-10 16:43:42 +0300

    Get ALL Patients

--- SmartClinic.Application/Features/Patients/Mapper/PatientMappingExtensions.cs
+++ SmartClinic.Application/Features/Patients/Mapper/PatientMappingExtensions.cs
@@ -1,35 +1,35 @@
-﻿using SmartClinic.Application.Features.Patients.Query.DTOs;
-using SmartClinic.Application.Features.Patients.Query.DTOs.GetPatient;
-using SmartClinic.Application.Features.Patients.Query.DTOs.GetPatients;
+﻿//using SmartClinic.Application.Features.Patients.Query.DTOs;
+//using SmartClinic.Application.Features.Patients.Query.DTOs.GetPatient;
+//using SmartClinic.Application.Features.Patients.Query.DTOs.GetPatients;
 
-namespace SmartClinic.Application.Mapping;
+//namespace SmartClinic.Application.Mapping;
 
-public static class PatientMappingExtensions
-{
-    public static GetAllPatientsResponse ToGetAllPatientsResponse(this Patient patient)
-    { 
-        return new GetAllPatientsResponse(
-            id: patient.Id,
-            firstName: patient.User?.FirstName ?? "",
-            lastName: patient.User?.LastName ?? "",
-            userPhoneNumber: patient.User?.PhoneNumber ?? "",
-            age: patient.User.Age 
-        );
-    }
+//public static class PatientMappingExtensions
+//{
+//    public static GetAllPatientsResponse ToGetAllPatientsResponse(this Patient patient)
+//    { 
+//        return new GetAllPatientsResponse(
+//            id: patient.Id,
+//            firstName: patient.User?.FirstName ?? "",
+//            lastName: patient.User?.LastName ?? "",
+//            userPhoneNumber: patient.User?.PhoneNumber ?? "",
+//            age: patient.User.Age 
+//        );
+//    }
 
-    public static GetPatientByIdResponse ToGetPatientByIdResponse(this Patient patient)
-    {
-        return new GetPatientByIdResponse(
-            id: patient.Id,
-            firstName: patient.User.FirstName,
-            lastName: patient.User.LastName,
-            userEmail: patient.User.Email,
-            userPhoneNumber: patient.User.PhoneNumber,
-            age: patient.User.Age,
-            address: patient.User.Address,
-            image: patient.User.ProfileImage, 
-            medicalHistory: patient.MedicalHistory 
-        );
-    }
+//    public static GetPatientByIdResponse ToGetPatientByIdResponse(this Patient patient)
+//    {
+//        return new GetPatientByIdResponse(
+//            id: patient.Id,
+//            firstName: patient.User.FirstName,
+//            lastName: patient.User.LastName,
+//            userEmail: patient.User.Email,
+//            userPhoneNumber: patient.User.PhoneNumber,
+//            age: patient.User.Age,
+//            address: patient.User.Address,
+//            image: patient.User.ProfileImage, 
+//            medicalHistory: patient.MedicalHistory 
+//        );
+//    }
 
-}
+//}
---
+++ SmartClinic.Application/Features/Patients/Query/GetPatients/GetAllPatientsValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace SmartClinic.Application.Features.Patients.Query.GetPatients
+{
+    public class GetAllPatientsValidator : AbstractValidator<GetAllPatientsParams>
+    {
+        public GetAllPatientsValidator()
+        {
+            RuleFor(x => x.PageIndex)
+                .GreaterThanOrEqualTo(1);
+
+            RuleFor(x => x.PageSize)
+                .GreaterThanOrEqualTo(1)
+                .LessThanOrEqualTo(20);
+
+            RuleFor(x => x.OrderBy)
+                .Must(ValidProperty).WithMessage("Invalid Property Name");
+        }
+
+        private bool ValidProperty(string? orderBy)
+        {
+            if (orderBy is null) return true;
+
+            return PagingValidator.IsValidProperty(orderBy, typeof(GetAllPatientsResponse));
+        }
+    }
+}
