3a993f0ca10828bddaa138f494a48cdf7cd9eb34
Gehad Shalaby <119525675+Gehad555@users.noreply.github.com>
2025-05-10 20:28:10 +0300

    Reslove

--- SmartClinic.Application/Features/Patients/Query/GetPatients/GetAllPatientsResponse.cs
+++ SmartClinic.Application/Features/Patients/Query/GetPatients/GetAllPatientsResponse.cs
@@ -1,21 +1,31 @@
-namespace SmartClinic.Application.Features.Doctors.Query.GetDoctors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class GetAllDoctorsResponse
+namespace SmartClinic.Application.Features.Patients.Query.GetPatients
 {
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string? LastName { get; set; }
-    public int Age { get; set; }
-    public string Image { get; set; }
-    public string Specialization { get; set; }
-    public string? Description { get; set; }
-    public int? WaitingTime { get; set; }
-
-
-
-    public GetAllDoctorsResponse()
+    public class GetAllPatientsResponse 
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserPhoneNumber { get; set; }
+        public int Age { get; set; }
 
+        public GetAllPatientsResponse()
+        {
+        }
+
+        public GetAllPatientsResponse(int id, string firstName, string lastName, string userPhoneNumber, int age)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            UserPhoneNumber = userPhoneNumber;
+            Age = age;
+        }
     }
 
 }
